f5f0f7990e79ff241c1d3352bdc4ffbe790e1b3e
li <li@iamli.cc>
2019-04-23 11:54:29 +0800

    First commit

---
+++ ida_address_convert/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ida_address_convert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(529, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "基址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(532, 152);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(131, 28);
+            this.address.TabIndex = 2;
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 15);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(500, 197);
+            this.input.TabIndex = 3;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 223);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(500, 197);
+            this.output.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 432);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ ida_address_convert/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ida_address_convert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string []a = input.Text.Split('\n');
+            string pattern = @"\b([0-9a-fA-F]+)\b";
+            output.Text = "";
+            foreach (string s in a)
+            {
+                MatchCollection t = Regex.Matches(a[0], pattern);
+                output.Text += string.Format("tb *0x{0}+0x{1}\r\n", address.Text, t[0].Value);
+            }
+            
+            
+        }
+    }
+}
